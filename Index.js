cc4d4b2eb8df8ca6f544d4e3721b913e08ffe7fa
Mila Pauline <milam.maurer30@gmail.com>
2025-12-16 08:04:52 +0100

    Ajout des modifications à la carte interactive Leaflet

--- Index.js
+++ Index.js
@@ -1,55 +1,74 @@
-//Initialisation de la carte centrée sur Lausanne
+// Initialisation de la carte
 var map = L.map('map').setView([46.519, 6.632], 13);
 
-// Ajout du fond de carte OpenStreetMap
+// Fond de carte OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Définir des icônes personnalisées pour chaque type de zone
-var parcIcon = L.icon({
+// Icônes personnalisées
+var HopitalIcon = L.icon({
   iconUrl: 'Icones/Hopital.png',
-  iconSize: [15, 20], 
-  iconAnchor: [14, 40], 
-  popupAnchor: [0, -40]
+  iconSize: [25, 30],
+  iconAnchor: [12, 30],
+  popupAnchor: [0, -30]
 });
 
-var reserveIcon = L.icon({
+var MedecinIcon = L.icon({
   iconUrl: 'Icones/Médecin.png',
-  iconSize: [15, 20],
-  iconAnchor: [14, 40],
-  popupAnchor: [0, -40]
+  iconSize: [25, 30],
+  iconAnchor: [12, 30],
+  popupAnchor: [0, -30]
 });
 
-var foretIcon = L.icon({
-  iconUrl: 'Icones/Pharmacie.png', 
-  iconSize: [15, 20],
-  iconAnchor: [14, 40],
-  popupAnchor: [0, -40]
+var PharmacieIcon = L.icon({
+  iconUrl: 'Icones/Pharmacie.png',
+  iconSize: [25, 30],
+  iconAnchor: [12, 30],
+  popupAnchor: [0, -30]
 });
 
-// Fonction pour ajouter une couche GeoJSON et configurer les popups
+// Fonction générique pour ajouter une couche GeoJSON
 function addLayer(layerData, color, icon) {
-  return L.geoJSON( layerData, {
-   style: function() {
-     return {
-     color: color,
-     weight: 1,
-     opacity: 1,
-     fillOpacity: 0.5,
-  };
-},
+  return L.geoJSON(layerData, {
+    style: function () {
+      return {
+        color: color,
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.5
+      };
+    },
 
-pointToLayer: function(feature, latlng) {
-  return L.marker(latlng, { icon: icon }); // Utilise l'icône personnalisée
-},
+    pointToLayer: function (feature, latlng) {
+      return L.marker(latlng, { icon: icon });
+    },
 
-onEachFeature: function(feature, layer) {
-  layer.on('click', function() {
-  console.log("Clicked on feature:", feature); // Log pour vérifier le contenu de la feature
+    onEachFeature: function (feature, layer) {
+      if (feature.properties) {
+        let popupContent = "<strong>" + (feature.properties.nom || "Sans nom") + "</strong>";
+        layer.bindPopup(popupContent);
+      }
+    }
+  });
+}
 
-// Définir les couches de chaque zone
-var HopitalLayer = addLayer(parcs, '#28a745', parcIcon);  // Parcs - Vert
-var MédecinLayer = addLayer(reserves, '#007bff', reserveIcon);  // Réserves marines - Bleu
-var PharmacieLayer = addLayer(forets, '#8B4513', foretIcon) ;  // Forêts - Rouge
+// Création des couches
+var HopitalLayer = addLayer(Hopital, '#28a745', HopitalIcon);
+var MedecinLayer = addLayer(Medecin, '#007bff', MedecinIcon);
+var PharmacieLayer = addLayer(Pharmacie, '#8B4513', PharmacieIcon);
+
+// Ajout des couches à la carte
+HopitalLayer.addTo(map);
+MedecinLayer.addTo(map);
+PharmacieLayer.addTo(map);
+
+// Contrôle des couches
+var overlayMaps = {
+  "Hôpitaux": HopitalLayer,
+  "Médecins": MedecinLayer,
+  "Pharmacies": PharmacieLayer
+};
+
+L.control.layers(null, overlayMaps).addTo(map);
